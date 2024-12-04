d88be4246cb2ce1f472e9c04ea142cdeba3efe66
raxeraditya <aditya1596k@gmail.com>
2024-12-04 17:39:09 +0530

    cosole

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,5 +4,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
+console.log("env key");
+console.log(supabaseUrl, supabaseAnonKey);
 export default supabase;
