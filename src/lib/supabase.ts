69dc1459678a4ca6c82ea2752daeb3ae290290be
raxeraditya <aditya1596k@gmail.com>
2024-12-04 17:58:18 +0530

    development

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,6 +4,10 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-console.log("env key");
+// Log only on the client side
+if (import.meta.env.MODE === "development") {
+  console.log("Supabase URL and Anon Key:");
+  console.log(supabaseUrl, supabaseAnonKey);
+}
 console.log(supabaseUrl, supabaseAnonKey);
 export default supabase;
